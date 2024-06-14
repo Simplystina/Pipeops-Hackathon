410a0623d29d5cca8a61464735b3a95b86952584
Simplystina <62130713+Simplystina@users.noreply.github.com>
2024-06-14 13:27:43 +0100

    wrote the function for creating an order

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,4 +1,5 @@
 import { ErrorResponse } from "../core";
+import OrderModel from "../models/Order";
 import generateUniqueInvitationCode from "../util/uniqueCode";
 
 
@@ -6,7 +7,14 @@ import generateUniqueInvitationCode from "../util/uniqueCode";
 const createAnOrder = async (data: any, id: string) => {
     console.log(data)
     const code = await generateUniqueInvitationCode()
-   
+    const dataToCreate = {
+        ...data,
+        paymentCode: code,
+        userId: id
+    }
+    const createdData = OrderModel.create(dataToCreate)
+    console.log(createdData)
+    return createdData
 };
 
 export default {
